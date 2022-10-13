01b2cd6979a7ffe7c1bab359a042b25c50f07f1b
Ivan <ivanbakalov15@gmail.com>
2022-10-13 13:37:09 +0200

    sdsd

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, flash
 import csv
 from random import randrange
 from turbo_flask import Turbo
-from turbo_flask import Turbo
 
 
 order = []
