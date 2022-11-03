121061e4d1ee2924dc61b5c16212336aa807e02e
Colin Bielsma <ctbielsma@gmail.com>
2022-11-03 09:09:23 +0100

    arduino

--- timer_temp.py
+++ timer_temp.py
@@ -1,3 +1,4 @@
+from dis import code_info
 import json
 from pkgutil import get_data
 import sys
@@ -10,11 +11,20 @@ from fhict_cb_01.CustomPymata4 import CustomPymata4
 BUTTON1PIN = 9
 BUTTON2PIN = 8
 LDRPIN = 2
+REDLEDPIN = 4
 DHTPIN  = 12
 
 humidity = 0
 temperature = 0
 t = 0
+countdown = 0
+timer = 15
+count = 0
+status = 0
+timersStarted = []
+orders = []
+array = {}
+
 
 def Measure(data):
     global humidity, temperature
@@ -22,62 +32,89 @@ def Measure(data):
         humidity = data[4]
         temperature = data[5]
 
+def Buttonchange(data):
+    global count, status, orders
+    if data[2] == 0:
+        if globals()["status" + orders[count][0]] == 0:
+            globals()["status" + orders[count][0]] += 1
+            timersStarted.append(orders[count][0])
+            board.digital_pin_write(REDLEDPIN, 1)
+        elif globals()["status" + orders[count][0]] == 2:
+            globals()["status" + orders[count][0]] += 1
+            orders.pop(count)
+        board.displayShow(orders[(count)][0])
+        print(orders[count][0])
+        print(globals()["status" + orders[count][0]])
+
+
+def Buttonchange2(data):
+    global count, orders
+    if data[2] == 0:
+        if count == (len(orders)-1):
+            count = 0
+        else:
+            count += 1
+        board.displayShow(orders[count][0])
+        if globals()["timer" + orders[count][0]] == 10 or globals()["timer" + orders[count][0]] == 0:
+            board.digital_pin_write(REDLEDPIN, 0) 
+        else:
+            board.digital_pin_write(REDLEDPIN, 1) 
+    print(data)
+
 def setup():
     global board
-    board = CustomPymata4(com_port = "COM3")
-    board.set_pin_mode_digital_input_pullup(BUTTON1PIN)
-    board.set_pin_mode_digital_input_pullup(BUTTON2PIN)
+    board = CustomPymata4(com_port = "COM7")
+    board.set_pin_mode_digital_input_pullup(BUTTON1PIN, callback=Buttonchange)
+    board.set_pin_mode_digital_input_pullup(BUTTON2PIN, callback=Buttonchange2)
     board.displayOn()
     board.set_pin_mode_dht(DHTPIN, sensor_type=11, differential=.05, callback=Measure)
+    board.set_pin_mode_digital_output(REDLEDPIN)
     time.sleep(2)
 
+def updateOrders():
+    global orders, array
+    array = {}
+    resp = requests.get('http://192.168.2.4:5000/testinGet')
+    pizzas = resp.content
+    orders = json.loads(pizzas)
+    for order in orders:
+        if ("status" + order[0]) in globals():
+            array["Status" + order[0]] = globals()["status" + order[0]]
+            array.update({"Timer" + order[0]: globals()["timer" + order[0]]})
+        else:
+            globals()["status" + order[0]] = 0
+            globals()["timer" + order[0]] = 10
+        print(array)
+    print(orders)
+
 def loop():
-    global humidity, temperature, orderstarted, t, response
-    
-    level1, time_stamp1 = board.digital_read(BUTTON1PIN)
-    level2, time_stamp2 = board.digital_read(BUTTON2PIN)
-    orderdone = True
-    if t > 2:
-        if level1 == 0:
-            timer = input("How much time do you want the pizza to be cooked for? Use s or m for seconds or minutes. ")
-            if 'm' in timer:
-                timer = timer.replace("m", "")
-                orderstarted = int(timer) * 60
-            elif 's' in timer:
-                timer = timer.replace('s', "")
-                orderstarted = int(timer)
+    global timer, countdown, array, response
+    countdown += 1
+    if countdown == 10:
+        countdown = 0
+        for timer in timersStarted:
+            if globals()["timer" + timer] != 0:
+                globals()["timer" + timer] -= 1
+                print(globals()["timer" + timer])
             else:
-                orderstarted = 60
-            print("Timer has begun!")
-            
-            while orderstarted:
-                board.displayShow(orderstarted)
-                time.sleep(1)
-                print(str(round(temperature)) + "C")
-                orderstarted -= 1
-                orderdone = False
-                array = {'status': 0, 'timer': orderstarted}
-                response = requests.post('http://127.0.0.1:5000/status', json = array)
-            print("Timer has ended!")
-            array["status"] = 1
-            response = requests.post('http://127.0.0.1:5000/status', json = array)
-        while orderdone == False:
-            level2, time_stamp2 = board.digital_read(BUTTON2PIN)
-            if level2 == 0:
-                orderdone = True
-                print("Order is ready!")
-                array["status"] = 2
-                response = requests.post('http://127.0.0.1:5000/status', json = array)
-                time.sleep(0.2)
-    t += 1
-                
+                globals()["status" + timer] = 2
+                timersStarted.remove(timer)
+                board.digital_pin_write(REDLEDPIN, 0)
+                print(globals()["status" + timer])
+        response = requests.post('http://192.168.2.4:5000/status', json = array)
+        updateOrders() 
+    time.sleep(0.1)
+
 
 
 setup()
+updateOrders()
+if orders != []:
+    board.displayShow(orders[0][0])
 while True:
     try:
         loop()
     except KeyboardInterrupt: # Shutdown Firmata on Crtl+C.
         print ('shutdown')
         board.shutdown()
-        sys.exit(0)  
+        sys.exit(0)
